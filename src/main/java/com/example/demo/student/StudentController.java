5b996a5140214cfe75233efa9cda21fbb72c8529
Rob Jewell <rob.jewell@ibotta.com>
2022-04-11 17:05:17 -0400

    student service

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -11,17 +11,17 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/v1/students")
 public class StudentController {
+
+  private final StudentService studentService;
+
+
+  public StudentController(StudentService studentService) {
+    this.studentService = studentService;
+  }
+
+
   @GetMapping
   public List<Student> getStudents() {
-    return List.of(
-            new Student(
-                    1L,
-                    "alice",
-                    "alice@gmail.com",
-                    LocalDate.of(2000, Month.JANUARY, 5),
-                    21
-            )
-
-    );
+    return studentService.getStudents();
   }
 }
